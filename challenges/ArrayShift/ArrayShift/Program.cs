65d54e6b899d0e53259fcd8c9a48e81b884d589f
Biniam <biniamt23@yahoo.com>
2019-10-17 13:43:17 -0700

    code challenge done

---
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] challenge = { 4, 5, 6, 7 };
+            int value = 88;
+            InsertShiftArray(challenge, value);
+        }
+
+        public static void InsertShiftArray(int[] challenge, int value)
+        {
+            int mid = challenge.Length / 2;
+            int[] newArray = new int[challenge.Length + 1];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (mid == i)
+                {
+                    newArray[i] = value;
+                }
+                else if (i > mid)
+                {
+                    newArray[i] = challenge[i-1];
+                }
+                else
+                {
+                    newArray[i] = challenge[i];
+                }
+            }
+
+          
+            Console.WriteLine("Your new array is: " + String.Join(" , ", newArray));
+        }
+
+    }
+}
