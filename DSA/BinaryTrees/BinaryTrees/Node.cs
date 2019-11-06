5be187739e700ee2df008cb20ae50d9324fc0623
Biniam <biniamt23@yahoo.com>
2019-11-05 22:13:53 -0800

    created classes, node class done

---
+++ DSA/BinaryTrees/BinaryTrees/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTrees
+{
+    /// <summary>
+    /// creating node class with generic type
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node <T> LeftChild { get; set; }
+        public Node <T> RightChild { get; set; }
+    }
+}
