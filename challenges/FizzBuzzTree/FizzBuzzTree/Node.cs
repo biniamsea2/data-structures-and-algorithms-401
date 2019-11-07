6ef8803ac12faee825032571fcf79508448392a9
Biniam <biniamt23@yahoo.com>
2019-11-07 13:42:14 -0800

    readme and node class created

---
+++ challenges/FizzBuzzTree/FizzBuzzTree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree
+{
+    class Node <Object>
+    {
+        public Object Value { get; set; }
+        public Node <Object> LeftChild { get; set; }
+        public Node <Object> RightChild { get; set; }
+
+        public Node(Object value)
+        {
+            Value = value;
+        }
+    }
+}
