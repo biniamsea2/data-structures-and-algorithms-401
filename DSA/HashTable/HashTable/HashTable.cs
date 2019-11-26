e34a4de516211b1a8ea8b3a44e96ad55e2bdb939
Biniam <biniamt23@yahoo.com>
2019-11-25 23:29:18 -0800

    everything setup for code challenge, not complete yet

---
+++ DSA/HashTable/HashTable/HashTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    class HashTable
+    {
+        public static void Add(string key, string value)
+        {
+
+        }
+
+        public static bool Contains(string key)
+        {
+            return true;
+
+        }
+
+        public static string Get(string key)
+        {
+            string value= "";
+            return value;
+
+        }
+
+        public static int Hash(string key)
+        {
+            int index = 1;
+            return index;
+
+        }
+
+
+    }
+}
